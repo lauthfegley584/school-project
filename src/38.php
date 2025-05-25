c5448ff551265b47cd5da5b4c8188fd3d3bf80b2
lauthfegley584 <lauthfegley584@gmail.com>
2025-05-25 08:31:48 +0000

    Add 38.php

---
+++ src/38.php
@@ -0,0 +1,12 @@
+<?php
+// 设置错误处理级别为警告或以上
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// 定义变量，这里假设你的项目代码中存在
+$project_code = "Your project code here";
+
+// 在这里添加你的项目代码的执行部分
+
+echo $project_code;
+?>
